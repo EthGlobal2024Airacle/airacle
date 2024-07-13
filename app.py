7986d754b10b4687e28e58d068cfc695c573d09f
Eli Shalom <eli.shalom@gmail.com>
2024-07-13 13:17:46 +0300

    Connected the investigation process to the app endpoint

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask, send_from_directory, request, redirect
 import os
 import random
 
+from experiments.address_fetcher import Investigator
+from experiments.blockchain_analyzer import BlockchainAnalyzer
+from experiments.blockchain_data_fetcher import BlockchainDataFetcher
 
 app = Flask(__name__)
+
 
 @app.before_request
 def before_request():
@@ -11,13 +15,21 @@ def before_request():
         url = request.url.replace("http://", "https://", 1)
         return redirect(url, code=301)
 
+
 @app.route('/')
 def serve_index():
     return send_from_directory('.', 'index.html')
 
-@app.route('/claimCheck')
-def get_claim_bot_check():
-    return str(random.randint(0, 100))
+
+@app.route('/claimCheck/<address>')
+def get_claim_bot_check(address: str):
+    fetcher = BlockchainDataFetcher()
+    analyzer = BlockchainAnalyzer(address, fetcher)
+    analyzer.analyze(max_queries=4)
+    subgraph = analyzer.get_community_subgraph()
+    investigator = Investigator(subgraph, fetcher)
+    investigator.enrich_community()
+    return investigator.evaluate_farmness(address)
 
 
 if __name__ == '__main__':
